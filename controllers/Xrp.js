34cbd6442d997b44566ca25b085f82fabcec6813
zyb955140 <384815317@qq.com>
2019-02-28 14:25:22 +0800

    更新转账方法

--- controllers/Xrp.js
+++ controllers/Xrp.js
@@ -1,8 +1,4 @@
-var express = require('express');
-var app = express();
-let router = require("./router/router")
-let path = require("path")
-var XrpWallet = require("./controllers/XrpTransaction")
+var XrpWallet = require('./XrpTransaction');
 const srcAddres='rUg8zSZMqT7DCN7ff9Sjv3HA5Vm2c7jDDj'; //Edison钱包地址 也是被监听的账户
 const secret = "snMJDHfumYZJiahem5T1n1r4w5RtC";  //Edison钱包密钥
 //const srcAddres='rQhsWVT8ftfaizQThZqYpLNBwsyHEcv77v';
@@ -15,14 +11,3 @@ pXrp.xrpInit(); //初始化，并开启监听
 //pXrp.xrpGetTransactions(function(result){console.log(result)},function (err) {console.log(err)},options); //测试获取历史记录
 pXrp.xrpGetBalances(function(result){console.log(result)},function (err) {console.log(err)}); //查询账户余额
 pXrp.xrpPayment(desAddres,2,false,function(result){console.log(result)},function (err){console.log("call payment error: " + err)}) //测试转账
-
-
-app.use(express.urlencoded({ extended: false }))
-app.engine('.html', require('ejs').__express);
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, "static")));
-
-app.use('/', router);
-
-console.log("正在监听3000端口")
-app.listen(3000)
